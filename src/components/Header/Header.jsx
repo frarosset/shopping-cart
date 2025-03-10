6700adda1dd8f2fda6db188f1cc55c9b9a6ebc08
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-10 19:24:50 +0100

    feat(Header): add (unfunctional) search, profile, watchlist and cart buttons to Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,12 @@ function Header() {
     <header>
       <h1>{data.shopName}</h1>
       <nav>nav (TODO)</nav>
+      <div>
+        <button aria-label="search">Search</button>
+        <button aria-label="profile">Profile</button>
+        <button aria-label="watchlist">Watchlist</button>
+        <button aria-label="cart">Cart</button>
+      </div>
     </header>
   );
 }
