92de6dc833e6af4bc3b4210f955e9186f80ff894
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-11 10:05:32 +0100

    feat(Header): instantiate Header component in the App component to style it

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "./components/Header/Header.jsx";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
