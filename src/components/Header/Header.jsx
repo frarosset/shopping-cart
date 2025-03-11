06dea4aa778c92ee1934e7021c22d8552709f936
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-11 10:12:05 +0100

    style(Header): add basic style to Header component through Styled Components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
 import data from "../../assets/data.json";
+import styled from "styled-components";
 
-function Header() {
+function Header({ className = "page-header" }) {
   return (
-    <header>
+    <header className={className}>
       <h1>{data.shopName}</h1>
       <nav>nav (TODO)</nav>
       <div>
@@ -15,4 +16,27 @@ function Header() {
   );
 }
 
-export default Header;
+const StyledHeader = styled(Header)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+  gap: var(--header-gap);
+  padding: var(--header-padding);
+  color: var(--header-col);
+
+  & > h1 {
+    font-size: var(--header-h1-fontsize);
+    font-family: var(--header-h1-font);
+  }
+
+  & > nav {
+    border-left: 1px solid var(--header-col);
+    padding-left: var(--header-gap);
+    margin-right: auto;
+  }
+`;
+
+export default StyledHeader; // export the styled component
