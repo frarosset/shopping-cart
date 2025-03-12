54a25c8cdba6c512ac0f1cbf7652a6873e609ac9
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-12 14:18:23 +0100

    test(CategorySectionList): check that CategorySectionList component renders a list of category sections provided via props

--- src/components/Shop/CategorySectionList.test.jsx
+++ src/components/Shop/CategorySectionList.test.jsx
@@ -1,9 +1,10 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import CategorySectionList from "./CategorySectionList.jsx";
+import data from "../../assets/data.json";
 
 const setup = () => ({
-  ...render(<CategorySectionList />),
+  ...render(<CategorySectionList sectionList={data.sectionList} />),
 });
 
 describe("CategorySectionList", () => {
@@ -13,5 +14,19 @@ describe("CategorySectionList", () => {
     const categorySectionList = screen.getByRole("list");
 
     expect(categorySectionList).toBeInTheDocument();
+  });
+
+  it("renders a list of category sections", () => {
+    setup();
+
+    const sectionItems = screen.getAllByRole("listitem");
+    expect(sectionItems.length).toBe(data.sectionList.length);
+
+    data.sectionList.forEach((section) => {
+      const sectionItem = sectionItems.find(
+        (item) => item.textContent === section
+      );
+      expect(sectionItem).toBeInTheDocument();
+    });
   });
 });
