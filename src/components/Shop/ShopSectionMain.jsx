c2b429e485906b39590bb8a58d2f1d4d04178ce5
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-13 17:09:51 +0100

    feat(ShopSectionMain): add heading of the associated section to ShopSectionMain component

--- src/components/Shop/ShopSectionMain.jsx
+++ src/components/Shop/ShopSectionMain.jsx
@@ -1,14 +1,22 @@
 import { useParams } from "react-router-dom";
+import data from "../../assets/data.json";
+
+const getSectionData = (section) => data.sections[section];
 
 function ShopSectionMain({ className = "" }) {
   const { section } = useParams();
+
+  const sectionData = getSectionData(section);
+  const sectionName = sectionData.name;
 
   return (
     <main
       className={`shop-section-main ${className}`}
       data-testid="shop-section-main"
     >
-      {section}
+      <header>
+        <h2>{sectionName}</h2>
+      </header>
     </main>
   );
 }
