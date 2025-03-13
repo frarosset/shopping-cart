460a61e2b46a8a7f18c68a89f5fd5a6175e158d7
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-13 14:50:13 +0100

    fix(ShopMain): fix test for ShopMain component by mocking the CategorySectionList component

--- src/components/Shop/ShopMain.test.jsx
+++ src/components/Shop/ShopMain.test.jsx
@@ -1,6 +1,21 @@
-import { describe, it, expect } from "vitest";
+import { vi, describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import ShopMain from "./ShopMain.jsx";
+import data from "../../assets/data.json";
+
+const sectionList = data.sectionList;
+
+const mockCategorySectionList = vi.fn();
+vi.mock("./CategorySectionList.jsx", () => ({
+  default: (props) => {
+    mockCategorySectionList(props);
+    return (
+      <div data-testid="__category-section-list__">
+        {JSON.stringify(props.sectionList)}
+      </div>
+    );
+  },
+}));
 
 const setup = () => {
   return {
@@ -9,11 +24,14 @@ const setup = () => {
 };
 
 describe("ShopMain", () => {
-  it("correctly renders the component", () => {
+  it("correctly renders the component rendering a categorySectionList component", () => {
     setup();
 
-    const categorySectionList = screen.getByTestId("category-section-list");
+    const categorySectionList = screen.getByTestId("__category-section-list__");
 
     expect(categorySectionList).toBeInTheDocument();
+    expect(mockCategorySectionList).toHaveBeenCalledExactlyOnceWith({
+      sectionList: sectionList,
+    });
   });
 });
