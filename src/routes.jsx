d9ed604a6705f2aaf1a3b14e29ef6bfca1e1eab4
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-13 18:51:53 +0100

    feat(App): render ShopCategoryMain component in App component's Outlet in 'shop/c/:category' route

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import App from "./App.jsx";
 import ShopMain from "./components/Shop/ShopMain.jsx";
 import ShopSectionMain from "./components/Shop/ShopSectionMain.jsx";
+import ShopCategoryMain from "./components/Shop/ShopCategoryMain.jsx";
 
 const routes = [
   {
@@ -10,7 +11,7 @@ const routes = [
       { path: "/", element: <div>Home</div> },
       { path: "/shop", element: <ShopMain /> },
       { path: "/shop/:section", element: <ShopSectionMain /> },
-      { path: "/shop/c/:category", element: <div>Category</div> },
+      { path: "/shop/c/:category", element: <ShopCategoryMain /> },
       { path: "/about", element: <div>About</div> },
     ],
   },
