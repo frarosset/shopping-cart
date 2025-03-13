4a075b02074e109d7e4ca5c5ab0129f4b411efb2
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-13 18:20:29 +0100

    feat(ShopCategoryMain): add heading of the associated category to ShopCategoryMain component

--- src/components/Shop/ShopCategoryMain.jsx
+++ src/components/Shop/ShopCategoryMain.jsx
@@ -1,14 +1,22 @@
 import { useParams } from "react-router-dom";
+import data from "../../assets/data.json";
+
+const getCategoryData = (category) => data.categories[category];
 
 function ShopCategoryMain({ className = "" }) {
   const { category } = useParams();
+
+  const categoryData = getCategoryData(category);
+  const categoryName = categoryData.name;
 
   return (
     <main
       className={`shop-category-main ${className}`}
       data-testid="shop-category-main"
     >
-      {category}
+      <header>
+        <h2>{categoryName}</h2>
+      </header>
     </main>
   );
 }
