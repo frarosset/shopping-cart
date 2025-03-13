1381688e7d5a8fc3bffea8db740848cfe5191a59
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-13 17:35:24 +0100

    feat(ShopSectionMain): render a list of categories associated with the section in ShopSectionMain component

--- src/components/Shop/ShopSectionMain.jsx
+++ src/components/Shop/ShopSectionMain.jsx
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom";
+import CategoryList from "./CategoryList.jsx";
 import data from "../../assets/data.json";
 
 const getSectionData = (section) => data.sections[section];
@@ -8,6 +9,7 @@ function ShopSectionMain({ className = "" }) {
 
   const sectionData = getSectionData(section);
   const sectionName = sectionData.name;
+  const sectionCategories = sectionData.categories;
 
   return (
     <main
@@ -16,6 +18,7 @@ function ShopSectionMain({ className = "" }) {
     >
       <header>
         <h2>{sectionName}</h2>
+        <CategoryList categoryList={sectionCategories} />
       </header>
     </main>
   );
