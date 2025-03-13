f14694b2bf32d072829a13d22ef8f4c9c17ef4d6
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-13 11:29:31 +0100

    test(CategorySectionItem): checks that the CategorySectionItem component renders a list of categories associated with the section

--- src/components/Shop/CategorySectionItem.test.jsx
+++ src/components/Shop/CategorySectionItem.test.jsx
@@ -1,10 +1,29 @@
-import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { vi, describe, it, expect, beforeEach } from "vitest";
+import { render, screen, within } from "@testing-library/react";
 import CategorySectionItem from "./CategorySectionItem.jsx";
 import data from "../../assets/data.json";
 
 const section = data.sectionList[0];
 const sectionData = data.sections[section];
+
+const mockCategoryList = vi.fn();
+vi.mock("./CategoryList.jsx", () => ({
+  default: (props) => {
+    mockCategoryList(props);
+    return (
+      <ul>
+        {props.categoryList.map((cat) => (
+          <li data-testid="__category-item__">{cat}</li>
+        ))}
+      </ul>
+    );
+  },
+}));
+
+/* mocks are hoisted: reset them before each test */
+beforeEach(() => {
+  vi.resetAllMocks();
+});
 
 const setup = () => ({
   ...render(<CategorySectionItem section={section} />),
@@ -19,5 +38,17 @@ describe("CategorySectionItem", () => {
     });
 
     expect(categoryName).toBeInTheDocument();
+  });
+
+  it("renders a list of categories associated with the section", () => {
+    setup();
+
+    const list = screen.getByRole("list");
+    const categoryItems = within(list).getAllByTestId("__category-item__");
+
+    expect(mockCategoryList).toHaveBeenCalledExactlyOnceWith({
+      categoryList: sectionData.categories,
+    });
+    expect(categoryItems.length).toBe(sectionData.categories.length);
   });
 });
