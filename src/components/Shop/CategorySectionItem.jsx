bbacd47421318628c881f4e5da9979fdb4bb0987
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-13 11:30:08 +0100

    feat(CategorySectionItem): renders a CategoryList of the categories associated with the section in the CategorySectionItem component

--- src/components/Shop/CategorySectionItem.jsx
+++ src/components/Shop/CategorySectionItem.jsx
@@ -1,14 +1,19 @@
+import CategoryList from "./CategoryList.jsx";
 import data from "../../assets/data.json";
-const sectionData = (section) => data.sections[section];
+const getSectionData = (section) => data.sections[section];
 
 function CategorySectionItem({ section, className = "" }) {
-  const sectionName = sectionData(section).name;
+  const sectionData = getSectionData(section);
+  const sectionName = sectionData.name;
+  const sectionCategories = sectionData.categories;
+
   return (
     <div
       className={`category-section-item ${className}`}
       data-testid="category-section-item"
     >
       <h2>{sectionName}</h2>
+      <CategoryList categoryList={sectionCategories} />
     </div>
   );
 }
