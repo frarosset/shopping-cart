df89adb2153e06451aa42e87e21f33c8da59025b
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-13 18:19:55 +0100

    test(ShopCategoryMain): check that ShopCategoryMain component renders a heading of the associated category

--- src/components/Shop/ShopCategoryMain.test.jsx
+++ src/components/Shop/ShopCategoryMain.test.jsx
@@ -1,9 +1,10 @@
 import { vi, describe, it, expect, beforeEach } from "vitest";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import ShopCategoryMain from "./ShopCategoryMain.jsx";
 import data from "../../assets/data.json";
 
 const sampleCategory = Object.keys(data.categories)[0];
+const sampleCategoryName = data.categories[sampleCategory].name;
 
 const mockUseParams = vi.fn();
 vi.mock("react-router-dom", () => ({
@@ -29,5 +30,13 @@ describe("ShopCategoryMain", () => {
     setup();
 
     expect(mockUseParams).toHaveBeenCalledOnce();
+  });
+
+  it("renders a heading of the associated category", () => {
+    setup();
+
+    const heading = screen.getByRole("heading", { name: sampleCategoryName });
+
+    expect(heading).toBeInTheDocument();
   });
 });
