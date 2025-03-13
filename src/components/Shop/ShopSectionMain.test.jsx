df2b241513c899860f4159cd1203ae6d53386d96
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-13 17:34:08 +0100

    test(ShopSectionMain): check that ShopSectionMain component renders a list of categories associated with the section

--- src/components/Shop/ShopSectionMain.test.jsx
+++ src/components/Shop/ShopSectionMain.test.jsx
@@ -1,16 +1,33 @@
 import { vi, describe, it, expect, beforeEach } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import ShopSectionMain from "./ShopSectionMain.jsx";
 import data from "../../assets/data.json";
 
 const sampleSection = Object.keys(data.sections)[0];
 const sampleSectionName = data.sections[sampleSection].name;
+const sampleSectionData = data.sections[sampleSection];
 
 const mockUseParams = vi.fn();
 vi.mock("react-router-dom", () => ({
   useParams: (props) => {
     mockUseParams(props);
     return { section: sampleSection };
+  },
+}));
+
+const mockCategoryList = vi.fn();
+vi.mock("./CategoryList.jsx", () => ({
+  default: (props) => {
+    mockCategoryList(props);
+    return (
+      <ul>
+        {props.categoryList.map((cat, idx) => (
+          <li data-testid="__category-item__" key={idx}>
+            {cat}
+          </li>
+        ))}
+      </ul>
+    );
   },
 }));
 
@@ -38,5 +55,17 @@ describe("ShopSectionMain", () => {
     const heading = screen.getByRole("heading", { name: sampleSectionName });
 
     expect(heading).toBeInTheDocument();
+  });
+
+  it("renders a list of categories associated with the section", () => {
+    setup();
+
+    const list = screen.getByRole("list");
+    const categoryItems = within(list).getAllByTestId("__category-item__");
+
+    expect(mockCategoryList).toHaveBeenCalledExactlyOnceWith({
+      categoryList: sampleSectionData.categories,
+    });
+    expect(categoryItems.length).toBe(sampleSectionData.categories.length);
   });
 });
