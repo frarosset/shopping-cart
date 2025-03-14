afe3f22945d97c2d854148a8b61128a6c2842829
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-14 15:09:32 +0100

    feat(getApiUrl): add possibility to provide queries to getSingleProductApiUrl() utility
    Queries are provided as an object, eg: { select: "title,price" }, which
    gets translated to "?select=title,price"
    
    getSingleProductApiUrl() is an utility exported by the getApiUrl module.

--- src/fetching-utils/getApiUrl.js
+++ src/fetching-utils/getApiUrl.js
@@ -1,7 +1,21 @@
 const baseApiUrl = "https://dummyjson.com/products";
 
-const getSingleProductApiUrl = (id) => {
-  return `${baseApiUrl}/${id}`;
+const queriesToString = (queries = {}) => {
+  const sep = (first) => (first ? "?" : "&");
+  const keys = Object.keys(queries);
+
+  const str = keys.reduce((str, key, idx) => {
+    const val = queries[key];
+    str += `${sep(idx == 0)}${key}=${val}`;
+    return str;
+  }, "");
+
+  return str;
+};
+
+const getSingleProductApiUrl = (id, queries = {}) => {
+  const queriesStr = queriesToString(queries);
+  return `${baseApiUrl}/${id}${queriesStr}`;
 };
 
 export { getSingleProductApiUrl };
