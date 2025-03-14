d2f003325fe4f63bd4cd6c68635aa2dff27fc687
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-14 15:05:36 +0100

    test(getApiUrl): check that getSingleProductApiUrl() utility returns the correct api url when an id and select query are provided
    getSingleProductApiUrl() is an utility exported by the getApiUrl module.

--- src/fetching-utils/getApiUrl.test.js
+++ src/fetching-utils/getApiUrl.test.js
@@ -4,9 +4,17 @@ import { getSingleProductApiUrl } from "./getApiUrl.js";
 describe("getApiUrl", () => {
   describe("getSingleProductApiUrl", () => {
     const productId = 3;
+    const queries = { select: "title,price" };
 
     it(`returns the correct api url when an id (${productId}) is provided`, () => {
       const apiUrl = getSingleProductApiUrl(productId);
+      expect(apiUrl).toMatchSnapshot();
+    });
+
+    it(`returns the correct api url when an id (${productId}) and a select query (${JSON.stringify(
+      queries
+    )}) are provided`, () => {
+      const apiUrl = getSingleProductApiUrl(productId, queries);
       expect(apiUrl).toMatchSnapshot();
     });
   });
