89558a2d8f0c7407244cf76bdb69e4e83b9601fb
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-17 18:32:03 +0100

    feat(ProductItem): add full price to ProductItem component

--- src/components/Shop/ProductItem.jsx
+++ src/components/Shop/ProductItem.jsx
@@ -3,12 +3,15 @@ import { Link } from "react-router-dom";
 function ProductItem({ productData, className = "" }) {
   return (
     <div className={`product-item ${className}`} data-testid="product-item">
-      {
-        <Link to={`/shop/p/${productData.id}`}>
-          <img src={productData.thumbnail} alt={productData.title} />
-          {<h3>{productData.title}</h3>}
-        </Link>
-      }
+      <Link to={`/shop/p/${productData.id}`}>
+        <img
+          className="thumbnail"
+          src={productData.thumbnail}
+          alt={productData.title}
+        />
+        {<h3 className="title">{productData.title}</h3>}
+      </Link>
+      <span className="full-price">{productData.price} €</span>
     </div>
   );
 }
