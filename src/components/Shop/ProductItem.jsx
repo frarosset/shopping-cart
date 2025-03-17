960d7a599d0c559cb25412412bf4d57550305041
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-17 18:12:55 +0100

    feat(ProductItem): add thumbnail to ProductItem component

--- src/components/Shop/ProductItem.jsx
+++ src/components/Shop/ProductItem.jsx
@@ -5,6 +5,7 @@ function ProductItem({ productData, className = "" }) {
     <div className={`product-item ${className}`} data-testid="product-item">
       {
         <Link to={`/shop/p/${productData.id}`}>
+          <img src={productData.thumbnail} alt={productData.title} />
           {<h3>{productData.title}</h3>}
         </Link>
       }
