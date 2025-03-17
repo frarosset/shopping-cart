a2a42ae987786628089b7fd2cf34f9394348d495
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-17 17:19:43 +0100

    test(ProductItem): check that ProductItem component renders a link to /shop/p/:productId page

--- src/components/Shop/__tests__/ProductItem.test.jsx
+++ src/components/Shop/__tests__/ProductItem.test.jsx
@@ -1,13 +1,17 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
 import ProductItem from "../ProductItem.jsx";
 
 const productData = {
+  id: 10,
   title: "Product Title",
 };
 
 const setup = () => ({
-  ...render(<ProductItem productData={productData} />),
+  ...render(<ProductItem productData={productData} />, {
+    wrapper: MemoryRouter,
+  }),
 });
 
 describe("ProductItem", () => {
@@ -19,5 +23,16 @@ describe("ProductItem", () => {
     });
 
     expect(productTitle).toBeInTheDocument();
+  });
+
+  it("renders a link to /shop/p/:productId page", () => {
+    setup();
+
+    const routeTo = `shop/p/${productData.id}`;
+    const basePath = window.location.href;
+    const link = screen.getByRole("link");
+
+    expect(link).toBeInTheDocument();
+    expect(link.href).toBe(`${basePath}${routeTo}`);
   });
 });
