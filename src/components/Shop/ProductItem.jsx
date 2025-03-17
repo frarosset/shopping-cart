db2876c1172ca7bf5e4fa3116918b61a25fa5622
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-17 17:20:56 +0100

    feat(ProductItem): add link to /shop/p/:product page to ProductItem component

--- src/components/Shop/ProductItem.jsx
+++ src/components/Shop/ProductItem.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 function ProductItem({ productData, className = "" }) {
   return (
     <div className={`product-item ${className}`} data-testid="product-item">
-      <h3>{productData.title}</h3>
+      {
+        <Link to={`/shop/p/${productData.id}`}>
+          {<h3>{productData.title}</h3>}
+        </Link>
+      }
     </div>
   );
 }
