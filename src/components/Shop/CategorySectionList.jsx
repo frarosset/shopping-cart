dd48646879d0e6aa877681d3fad40cbb3a222693
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-20 15:10:03 +0100

    style(CategorySectionList): add style to CategorySectionList component

--- src/components/Shop/CategorySectionList.jsx
+++ src/components/Shop/CategorySectionList.jsx
@@ -1,5 +1,6 @@
 import CategorySectionItem from "./CategorySectionItem.jsx";
 import List from "../Generic/List.jsx";
+import styled from "styled-components";
 
 function CategorySectionList({ sectionList, className = "" }) {
   const items = sectionList.map((section) => ({
@@ -8,12 +9,22 @@ function CategorySectionList({ sectionList, className = "" }) {
   }));
 
   return (
-    <List
+    <StyledCategorySectionList
       className={`category-section-list ${className}`}
       testId={"category-section-list"}
       items={items}
     />
   );
 }
+
+const StyledCategorySectionList = styled(List)`
+  display: flex;
+  flex-wrap: wrap;
+  padding: var(--section-list-padding);
+  gap: var(--section-list-gap);
+  width: 100%;
+
+  justify-content: center;
+`;
 
 export default CategorySectionList;
