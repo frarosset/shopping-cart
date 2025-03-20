c590a14da3f11253daa1ad2e5d1033ae327837b9
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-20 18:11:58 +0100

    test(CategorySectionItem): fix tests for CategorySectionItem
    The changes here reflect the changes in CategorySectionItem component to
    remove the CategoryList component.

--- src/components/Shop/__tests__/CategorySectionItem.test.jsx
+++ src/components/Shop/__tests__/CategorySectionItem.test.jsx
@@ -1,5 +1,5 @@
 import { vi, describe, it, expect, afterEach } from "vitest";
-import { render, screen, within } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import CategorySectionItem from "../CategorySectionItem.jsx";
 import data from "../../../assets/data.json";
@@ -7,21 +7,21 @@ import data from "../../../assets/data.json";
 const section = data.sectionList[0];
 const sectionData = data.sections[section];
 
-const mockCategoryList = vi.fn();
-vi.mock("../CategoryList.jsx", () => ({
-  default: (props) => {
-    mockCategoryList(props);
-    return (
-      <ul>
-        {props.categoryList.map((cat, idx) => (
-          <li data-testid="__category-item__" key={idx}>
-            {cat}
-          </li>
-        ))}
-      </ul>
-    );
-  },
-}));
+// const mockCategoryList = vi.fn();
+// vi.mock("../CategoryList.jsx", () => ({
+//   default: (props) => {
+//     mockCategoryList(props);
+//     return (
+//       <ul>
+//         {props.categoryList.map((cat, idx) => (
+//           <li data-testid="__category-item__" key={idx}>
+//             {cat}
+//           </li>
+//         ))}
+//       </ul>
+//     );
+//   },
+// }));
 
 /* mocks are hoisted: reset them before each test */
 afterEach(() => {
@@ -45,26 +45,31 @@ describe("CategorySectionItem", () => {
     expect(categoryName).toBeInTheDocument();
   });
 
-  it("renders a list of categories associated with the section", () => {
-    setup();
+  // Note: list removed
+  // it("renders a list of categories associated with the section", () => {
+  //   setup();
 
-    const list = screen.getByRole("list");
-    const categoryItems = within(list).getAllByTestId("__category-item__");
+  //   const list = screen.getByRole("list");
+  //   const categoryItems = within(list).getAllByTestId("__category-item__");
 
-    expect(mockCategoryList).toHaveBeenCalledExactlyOnceWith({
-      categoryList: sectionData.categories,
-    });
-    expect(categoryItems.length).toBe(sectionData.categories.length);
-  });
+  //   expect(mockCategoryList).toHaveBeenCalledExactlyOnceWith({
+  //     categoryList: sectionData.categories,
+  //   });
+  //   expect(categoryItems.length).toBe(sectionData.categories.length);
+  // });
 
   it("renders a link to /shop/:section page", () => {
     setup();
 
     const routeTo = `shop/${section}`;
     const basePath = window.location.href;
-    const link = screen.getByRole("link");
+    // note that there might be
+    const links = screen.getAllByRole("link", { title: sectionData.name });
 
-    expect(link).toBeInTheDocument();
-    expect(link.href).toBe(`${basePath}${routeTo}`);
+    expect(links.length).toBeOneOf([1, 2]); // there might be an extra link in an image
+    links.forEach((link) => {
+      expect(link).toBeInTheDocument();
+      expect(link.href).toBe(`${basePath}${routeTo}`);
+    });
   });
 });
