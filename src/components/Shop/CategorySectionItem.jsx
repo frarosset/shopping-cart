7baf5125881073c9f1173d4d7c99bf06550ea137
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-20 18:01:08 +0100

    style(CategorySectionItem): add style to CategorySectionItem component
    Note that some images have been added. The url of their source is
    grabbed from data.json, altough that filed has not been updated with
    that data, yet, so the text placeholder is shown.

--- src/components/Shop/CategorySectionItem.jsx
+++ src/components/Shop/CategorySectionItem.jsx
@@ -1,24 +1,88 @@
 import CategoryList from "./CategoryList.jsx";
 import { Link } from "react-router-dom";
+import Heading from "../Generic/Heading.jsx";
+import Image from "../Generic/Image.jsx";
 import data from "../../assets/data.json";
+import styled from "styled-components";
+
 const getSectionData = (section) => data.sections[section];
 
 function CategorySectionItem({ section, className = "" }) {
+  const hLevel = 2;
+
   const sectionData = getSectionData(section);
   const sectionName = sectionData.name;
-  const sectionCategories = sectionData.categories;
 
   return (
-    <div
+    <StyledCategorySectionItem
       className={`category-section-item ${className}`}
       data-testid="category-section-item"
     >
-      <h2>
+      <StyledImg
+        src={sectionData.image}
+        // alt={`${sectionName} image`}
+        // loading={"lazy"}
+      />
+      <StyledSectionHeading hLevel={hLevel}>
         <Link to={`/shop/${section}`}>{sectionName}</Link>
-      </h2>
-      <CategoryList categoryList={sectionCategories} />
-    </div>
+      </StyledSectionHeading>
+    </StyledCategorySectionItem>
   );
 }
+
+const StyledCategorySectionItem = styled.div`
+  position: relative;
+
+  width: var(--section-item-width);
+  height: var(--section-item-width);
+  border-radius: 50%;
+
+  overflow: hidden;
+
+  * {
+    transition: transform var(--section-transition-duration);
+  }
+`;
+
+const StyledSectionHeading = styled(Heading)`
+  height: 100%;
+  width: 100%;
+
+  z-index: 1;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  font-family: var(--heading-subtext-font);
+  font-size: var(--heading-subtext-font-size);
+  color: var(--col-txt-alt);
+
+  backdrop-filter: brightness(0.8);
+
+  & a {
+    width: 100%;
+    height: 100%;
+    padding: var(--small-gap);
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    text-transform: uppercase;
+    text-align: center;
+  }
+`;
+
+const StyledImg = styled(Image)`
+  object-fit: cover;
+  aspect-ratio: 1;
+  position: absolute;
+  inset: 0;
+
+  ${StyledCategorySectionItem}:hover & {
+    transform: scale(var(--section-item-hover-thumbnail-scale));
+  }
+`;
 
 export default CategorySectionItem;
