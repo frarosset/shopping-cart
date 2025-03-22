722385844f1b635f4d993ea56201e8fb871ac9e9
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-22 14:10:00 +0100

    feat(MessageWithImageBelow): add MessageWithImageBelow component
    This is a styled component which renders a p element that fills the
    container space.
    
    The components that are its children are rendered on the top.
    
    Below that, the picture from the url provided via imageUrl prop is shown,
    filling the whole space.

---
+++ src/components/Generic/MessageWithImageBelow.jsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+
+const MessageWithImageBelow = styled(({ children, imageUrl, className }) => {
+  return (
+    <StyledMessageWithImageBelow $imageUrl={imageUrl} className={className}>
+      {children}
+    </StyledMessageWithImageBelow>
+  );
+})``;
+
+const StyledMessageWithImageBelow = styled.p`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  height: 100%;
+  width: 100%;
+
+  &::after {
+    content: "";
+    flex: 1;
+    min-height: 0;
+    width: 100%;
+
+    background-image: url("${({ $imageUrl }) => $imageUrl}");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: contain;
+  }
+`;
+
+export default MessageWithImageBelow;
