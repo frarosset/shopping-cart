85211ee3437c4d970b436073f4aa528607c2d118
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-23 18:44:59 +0100

    feat(ErrorPage): make ErrorPage render error when location state contains error

--- src/components/Errors/ErrorPage.jsx
+++ src/components/Errors/ErrorPage.jsx
@@ -1,10 +1,16 @@
 import Header from "../Header/Header.jsx";
+import Error from "./Error.jsx";
 import { HeadingLevelContextProvider } from "../../contexts/HeadingLevelContext.jsx";
+import { useLocation } from "react-router-dom";
 
 function ErrorPage() {
+  // get current state (set by ErrorRedirect component)
+  const location = useLocation();
+
   return (
     <HeadingLevelContextProvider>
       <Header />
+      <Error error={location.state.error} />
     </HeadingLevelContextProvider>
   );
 }
