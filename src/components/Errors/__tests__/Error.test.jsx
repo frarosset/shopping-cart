6d79aebf66ac07e766787df02c6bac84eeb43337
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-23 16:43:40 +0100

    test(Error): check that Error component correctly renders the error image for 404 errors

--- src/components/Errors/__tests__/Error.test.jsx
+++ src/components/Errors/__tests__/Error.test.jsx
@@ -2,14 +2,26 @@ import { vi, describe, it, expect, afterEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Error from "../Error.jsx";
 
+const mockMessageWithImageBelow = vi.fn();
+vi.mock("../../Generic/MessageWithImageBelow", () => ({
+  default: (props) => {
+    mockMessageWithImageBelow(props);
+    return (
+      <div data-testid="__message-with-image-below__">
+        MessageWithImageBelow
+      </div>
+    );
+  },
+}));
+
 /* mocks are hoisted: reset them before each test */
 afterEach(() => {
   vi.resetAllMocks();
 });
 
-const setup = () => {
+const setup = (error) => {
   return {
-    ...render(<Error />),
+    ...render(<Error error={error} />),
   };
 };
 
@@ -23,6 +35,30 @@ describe("Error", () => {
     expect(heading).toBeInTheDocument();
     expect(subheading).toBeInTheDocument();
 
-    // some other custom message are present, but are dynamic and not tested
+    // some other custom message are present, but are dynamic and not tested here
+  });
+
+  it("correctly render the error image for 404 errors", () => {
+    setup({ status: 404 });
+
+    const image = screen.getByTestId("__message-with-image-below__");
+
+    expect(mockMessageWithImageBelow).toHaveBeenCalledExactlyOnceWith({
+      imageUrl: "/images/vector/404.jpg",
+    });
+
+    expect(image).toBeInTheDocument();
+  });
+
+  it("correctly render the error image for generic (non 404) errors", () => {
+    setup();
+
+    const image = screen.getByTestId("__message-with-image-below__");
+
+    expect(mockMessageWithImageBelow).toHaveBeenCalledExactlyOnceWith({
+      imageUrl: "/images/vector/error.jpg",
+    });
+
+    expect(image).toBeInTheDocument();
   });
 });
