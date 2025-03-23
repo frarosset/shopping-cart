163213c261aa0b65c23ba5757fc8cfe75acee84e
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-23 18:53:01 +0100

    feat(ErrorPage): make ErrorPage render Navigate to '/' when location state does not contain error

--- src/components/Errors/ErrorPage.jsx
+++ src/components/Errors/ErrorPage.jsx
@@ -1,11 +1,15 @@
 import Header from "../Header/Header.jsx";
 import Error from "./Error.jsx";
 import { HeadingLevelContextProvider } from "../../contexts/HeadingLevelContext.jsx";
-import { useLocation } from "react-router-dom";
+import { useLocation, Navigate } from "react-router-dom";
 
 function ErrorPage() {
   // get current state (set by ErrorRedirect component)
   const location = useLocation();
+
+  //   go to homepage if no error data is provided
+  if (!location.state || !location.state.error)
+    return <Navigate to="/" replace={true} />;
 
   return (
     <HeadingLevelContextProvider>
