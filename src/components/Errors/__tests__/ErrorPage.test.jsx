e869c63255784bd6d3e4c38fbcdaeec1245887ff
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-23 18:43:51 +0100

    test(ErrorPage): test that ErrorPage renders error when location state contains error

--- src/components/Errors/__tests__/ErrorPage.test.jsx
+++ src/components/Errors/__tests__/ErrorPage.test.jsx
@@ -1,6 +1,9 @@
 import { vi, describe, it, expect, afterEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import ErrorPage from "../ErrorPage.jsx";
+import { MemoryRouter } from "react-router";
+
+const sampleError = { error: { message: "Test Error Message" } };
 
 const mockHeader = vi.fn();
 vi.mock("../../Header/Header.jsx", () => ({
@@ -10,25 +13,47 @@ vi.mock("../../Header/Header.jsx", () => ({
   },
 }));
 
+const mockError = vi.fn();
+vi.mock("../Error.jsx", () => ({
+  default: (props) => {
+    mockError(props);
+    return <div data-testid="__error__">{props.error.message}</div>;
+  },
+}));
+
 /* mocks are hoisted: reset them before each test */
 afterEach(() => {
   vi.resetAllMocks();
 });
 
-const setup = () => {
+const setup = (state) => {
   return {
-    ...render(<ErrorPage />),
+    ...render(
+      <MemoryRouter initialEntries={[{ state: state }]}>
+        <ErrorPage />
+      </MemoryRouter>
+    ),
   };
 };
 
 describe("ErrorPage", () => {
   it("correctly renders Header component", () => {
-    setup();
+    setup(sampleError);
 
     const header = screen.getByTestId("__page-header__");
 
     expect(mockHeader).toHaveBeenCalledOnce();
 
     expect(header).toBeInTheDocument();
+  });
+
+  it("renders error when location state contains error", () => {
+    setup(sampleError);
+
+    const error = screen.getByTestId("__error__");
+
+    expect(mockError).toHaveBeenCalledExactlyOnceWith(sampleError);
+
+    expect(error).toBeInTheDocument();
   });
 });
