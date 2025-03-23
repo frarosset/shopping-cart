d0ac762c6aafdc8ca0cdf29af649c66d227406c4
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-23 19:13:50 +0100

    feat(ErrorPage): make ErrorPage clear route state on render

--- src/components/Errors/ErrorPage.jsx
+++ src/components/Errors/ErrorPage.jsx
@@ -7,6 +7,9 @@ function ErrorPage() {
   // get current state (set by ErrorRedirect component)
   const location = useLocation();
 
+  // clear state (so that page refresh redirect to homepage)
+  window.history.replaceState(null, null);
+
   //   go to homepage if no error data is provided
   if (!location.state || !location.state.error)
     return <Navigate to="/" replace={true} />;
