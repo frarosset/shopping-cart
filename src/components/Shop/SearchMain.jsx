e1ac82db07aeaa13bf6ab31bb43f05471f16b9d0
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-24 10:57:38 +0100

    feat(SearchMain): add a searchbox initialized with empty string to SearchMain component

--- src/components/Shop/SearchMain.jsx
+++ src/components/Shop/SearchMain.jsx
@@ -1,11 +1,24 @@
 import Heading from "../Generic/Heading.jsx";
+import { useState } from "react";
+import Input from "../Form/Input.jsx";
 
 function SearchMain({ className = "" }) {
+  const [query, setQuery] = useState("");
+
   return (
     <main className={className}>
       <header>
         <Heading>Search</Heading>
       </header>
+      <Input
+        id="query"
+        name="query"
+        value={query}
+        setValue={setQuery}
+        type="search"
+        maxLength={50}
+        placeholder="What are you looking for?"
+      />
     </main>
   );
 }
