7cdacabfe7b25da1ddde05e33aa0857bce545288
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-25 22:11:26 +0100

    feat(WishlistMain): add WishlistMain component that shows the current wishlist (if not empty)

---
+++ src/components/Shop/WishlistMain.jsx
@@ -0,0 +1,45 @@
+import Heading from "../Generic/Heading.jsx";
+import styled from "styled-components";
+import { useContext } from "react";
+import SavedProductsContext from "../../contexts/SavedProductsContext.jsx";
+import MessageWithImageBelow from "../Generic/MessageWithImageBelow.jsx";
+import ProductList from "./ProductList.jsx";
+import { Link } from "react-router-dom";
+
+function WishlistMain({ className = "" }) {
+  const { wishlist } = useContext(SavedProductsContext);
+
+  return (
+    <StyledMain className={className}>
+      <StyledHeader>
+        <Heading>Wishlist</Heading>
+      </StyledHeader>
+
+      {wishlist &&
+        (wishlist.length > 0 ? (
+          <ProductList productDataList={wishlist} />
+        ) : (
+          <MessageWithImageBelow imageUrl="/images/vector/empty-wishlist.jpg">
+            Your wishlist is empty!
+            <Link to="/shop">Go shopping now!</Link>
+          </MessageWithImageBelow>
+        ))}
+    </StyledMain>
+  );
+}
+
+const StyledHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 var(--page-padding-lr);
+`;
+
+const StyledMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  gap: var(--page-gap);
+  align-items: center;
+`;
+
+export default WishlistMain;
