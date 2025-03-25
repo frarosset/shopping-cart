c623fce58da461fa47285fa86bcf73c326a09838
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-25 09:30:40 +0100

    feat(HeartFillIcon): add HeartFillIcon component

---
+++ src/components/Icons/HeartFillIcon.jsx
@@ -0,0 +1,12 @@
+import Icon from "./Icon.jsx";
+import HeartFill from "../../assets/icons/heart-fill.svg?react";
+
+function HeartFillIcon() {
+  return (
+    <Icon>
+      <HeartFill />
+    </Icon>
+  );
+}
+
+export default HeartFillIcon;
