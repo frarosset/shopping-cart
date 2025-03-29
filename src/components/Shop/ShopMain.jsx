010c34bdaa0af4b1ca69adf91718f4d506eb1a73
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-29 17:09:46 +0100

    feat(ShopMain): render a ShopHighlightPresentation component in ShopMain component

--- src/components/Shop/ShopMain.jsx
+++ src/components/Shop/ShopMain.jsx
@@ -1,10 +1,12 @@
 import CategorySectionList from "./CategorySectionList.jsx";
+import ShopHighlightPresentation from "./ShopHighlightPresentation.jsx";
 import data from "../../assets/data.json";
 
 function ShopMain({ className = "" }) {
   return (
     <main className={`shop-main ${className}`} data-testid="shop-main">
       <CategorySectionList sectionList={data.sectionList} />
+      <ShopHighlightPresentation />
     </main>
   );
 }
