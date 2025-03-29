b3817b9be98b0443bd2ebe17cf356ea271300845
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-29 18:00:21 +0100

    fix(App): scroll to top at each location change

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import { Outlet } from "react-router-dom";
-import "./App.css";
+import { Outlet, useLocation, ScrollRestoration } from "react-router-dom";
+import { useEffect } from "react";
 import Header from "./components/Header/Header.jsx";
 import { HeadingLevelContextProvider } from "./contexts/HeadingLevelContext.jsx";
 import { SavedProductsContextProvider } from "./contexts/SavedProductsContext.jsx";
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    // scroll to top at each route change
+    const app = document.getElementById("root");
+    if (app) app.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <SavedProductsContextProvider>
       <HeadingLevelContextProvider>
