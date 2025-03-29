11a3ee62aacdd0f8b115a5fd7e5ba3d32fcb8a46
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-29 11:34:56 +0100

    test(SectionCategoriesPresentation): test that SectionCategoriesPresentation component renders a list of some products associated with each category

--- src/components/Shop/__tests__/SectionCategoriesPresentation.test.jsx
+++ src/components/Shop/__tests__/SectionCategoriesPresentation.test.jsx
@@ -1,11 +1,12 @@
-import { describe, it, expect } from "vitest";
+import { vi, describe, it, expect } from "vitest";
 import { render, screen, within } from "@testing-library/react";
 import SectionCategoriesPresentation from "../SectionCategoriesPresentation.jsx";
 import { HeadingLevelContextProvider } from "../../../contexts/HeadingLevelContext.jsx";
 import data from "../../../assets/data.json";
 
-const sampleCategories = Object.keys(data.categories);
+const sampleCategories = data.sections["electronics"].categories;
 const sampleCategoriesData = data.categories;
+const sampleCategoryUrl = (category) => `category/${category}/url/`;
 
 const setup = () => {
   return {
@@ -16,6 +17,26 @@ const setup = () => {
     ),
   };
 };
+
+const mockGetCategoryProductsApiUrl = vi.fn();
+vi.mock("../../../fetching-utils/getApiUrl.js", () => ({
+  getCategoryProductsApiUrl: (category) => {
+    mockGetCategoryProductsApiUrl(category);
+    return sampleCategoryUrl(category);
+  },
+}));
+
+const mockProductFetchList = vi.fn();
+vi.mock("../ProductFetchList.jsx", () => ({
+  default: ({ apiUrl, rowScroll }) => {
+    mockProductFetchList(apiUrl, rowScroll);
+    return (
+      <span
+        data-testid={`__product-fetch-list-${apiUrl}__`}
+      >{`Product Fetch List from ${apiUrl}`}</span>
+    );
+  },
+}));
 
 describe("SectionCategoriesPresentation", () => {
   it("renders a list of categories associated with the section", () => {
@@ -30,6 +51,35 @@ describe("SectionCategoriesPresentation", () => {
       });
 
       expect(heading).toBeInTheDocument();
+    });
+  });
+
+  it("renders a list of some products associated with each category", () => {
+    setup();
+
+    const list = screen.getByRole("list");
+    const liAll = within(list).getAllByRole("listitem");
+
+    // const productFetchList = screen.getAllByTestId("__product-fetch-list__");
+    const getCategoryProductsApiUrlCallArgs =
+      mockGetCategoryProductsApiUrl.mock.calls;
+
+    const productFetchListCallArgs = mockProductFetchList.mock.calls;
+
+    sampleCategories.forEach((category, idx) => {
+      const apiUrl = sampleCategoryUrl(category);
+
+      const productFetchList = within(liAll[idx]).getByTestId(
+        `__product-fetch-list-${apiUrl}__`
+      );
+
+      // Check that this call has been called with the correct args
+      expect(getCategoryProductsApiUrlCallArgs[idx]).toEqual([category]);
+
+      expect(productFetchListCallArgs[idx]).toEqual([apiUrl, true]);
+
+      // Check that the product list has been rendered in the document
+      expect(productFetchList).toBeInTheDocument();
     });
   });
 });
