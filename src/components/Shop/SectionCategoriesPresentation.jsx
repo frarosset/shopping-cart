858384321a5c39801e21c52521c6c6bcc3916a94
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-29 11:37:31 +0100

    feat(SectionCategoriesPresentation): render a list of some products associated with each category in SectionCategoriesPresentation component

--- src/components/Shop/SectionCategoriesPresentation.jsx
+++ src/components/Shop/SectionCategoriesPresentation.jsx
@@ -1,8 +1,33 @@
 import Heading from "../Generic/Heading.jsx";
+import useSortBy from "../../custom-hooks/useSortBy.jsx";
+import ProductFetchList from "./ProductFetchList.jsx";
+import { getCategoryProductsApiUrl } from "../../fetching-utils/getApiUrl.js";
 import { HeadingLevelContextProvider } from "../../contexts/HeadingLevelContext.jsx";
 import data from "../../assets/data.json";
 
+const productsLimit = 10;
+const productDataKeys = [
+  "title",
+  "price",
+  "discountPercentage",
+  "rating",
+  "thumbnail",
+  "availabilityStatus",
+  "stock",
+];
+const productKeysStr = productDataKeys.join(",");
+
 function SectionCategoriesPresentation({ sectionCategories, className = "" }) {
+  const { sortBy, order } = useSortBy(); // get default values
+
+  const getApiUrl = (category) =>
+    getCategoryProductsApiUrl(category, {
+      select: productKeysStr,
+      sortBy: sortBy,
+      order: order,
+      limit: productsLimit,
+    });
+
   return (
     <HeadingLevelContextProvider>
       <ul className={className}>
@@ -10,10 +35,11 @@ function SectionCategoriesPresentation({ sectionCategories, className = "" }) {
           const categoryName = data.categories[category].name;
 
           return (
-            <li>
+            <li key={category}>
               <header>
                 <Heading>{categoryName}</Heading>
               </header>
+              <ProductFetchList apiUrl={getApiUrl(category)} rowScroll={true} />
             </li>
           );
         })}
