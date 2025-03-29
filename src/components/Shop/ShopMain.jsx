4414163b458d4ebba34d6690d1da3b9bf1394278
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-29 17:10:38 +0100

    style(ShopMain): edit style of ShopMain component

--- src/components/Shop/ShopMain.jsx
+++ src/components/Shop/ShopMain.jsx
@@ -1,14 +1,22 @@
 import CategorySectionList from "./CategorySectionList.jsx";
 import ShopHighlightPresentation from "./ShopHighlightPresentation.jsx";
+import styled from "styled-components";
 import data from "../../assets/data.json";
 
 function ShopMain({ className = "" }) {
   return (
-    <main className={`shop-main ${className}`} data-testid="shop-main">
+    <StyledMain className={`shop-main ${className}`} data-testid="shop-main">
       <CategorySectionList sectionList={data.sectionList} />
       <ShopHighlightPresentation />
-    </main>
+    </StyledMain>
   );
 }
+
+const StyledMain = styled.main`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: var(--page-gap);
+`;
 
 export default ShopMain;
