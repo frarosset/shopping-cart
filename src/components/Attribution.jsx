58653dbc7e9d0532a88f2c9850a96549962561f8
frarosset <37275581+frarosset@users.noreply.github.com>
2025-03-30 13:42:43 +0200

    feat(Attribution): add Attribution component

---
+++ src/components/Attribution.jsx
@@ -0,0 +1,34 @@
+function Attribution() {
+  return (
+    <div>
+      <p>
+        Product data provided by the
+        <a href={"https://dummyjson.com/docs/products"} target="_blank">
+          DummyJSON API (products endpoint)
+        </a>
+        . Background images are from
+        <a href="https://www.freepik.com" target="_blank">
+          Freepik
+        </a>
+        . Vector images are from
+        <a
+          href="https://www.vecteezy.com/free-vector/not-found"
+          target="_blank"
+        >
+          Vecteezy
+        </a>
+        . Icons are from
+        <a href="https://phosphoricons.com/" target="_blank">
+          Phosphor icons
+        </a>
+        . Spinner Icon is from
+        <a href="https://github.com/n3r4zzurr0/svg-spinners" target="_blank">
+          SVG Spinners Link
+        </a>
+        .
+      </p>
+    </div>
+  );
+}
+
+export default Attribution;
