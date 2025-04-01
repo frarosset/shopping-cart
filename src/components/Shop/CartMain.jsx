c1530a0192f776f07b5044f4eedb9c6f86d8e52a
frarosset <37275581+frarosset@users.noreply.github.com>
2025-04-01 10:57:44 +0200

    feat(CartMain): renders an empty cart message and a link to the shop page when empty cart in CartMain component
    It is the same of what has been done for the WishlistMain component.

--- src/components/Shop/CartMain.jsx
+++ src/components/Shop/CartMain.jsx
@@ -1,11 +1,27 @@
 import Heading from "../Generic/Heading.jsx";
+import { useContext } from "react";
+import SavedProductsContext from "../../contexts/SavedProductsContext.jsx";
+import MessageWithImageBelow from "../Generic/MessageWithImageBelow.jsx";
+import { Link } from "react-router-dom";
 
 function CartMain({ className = "" }) {
+  const { cart } = useContext(SavedProductsContext);
+
   return (
     <main className={className}>
       <header>
         <Heading>Cart</Heading>
       </header>
+
+      {cart &&
+        (cart.length > 0 ? (
+          "SHOW CART (TODO)"
+        ) : (
+          <MessageWithImageBelow imageUrl="/images/vector/empty-cart.jpg">
+            Your cart is empty!
+            <Link to="/shop">Go shopping now!</Link>
+          </MessageWithImageBelow>
+        ))}
     </main>
   );
 }
