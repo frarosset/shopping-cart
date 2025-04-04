38544a0f1353382ef259a927f99f3b27a02492c2
frarosset <37275581+frarosset@users.noreply.github.com>
2025-04-04 12:27:06 +0200

    feat(CartMain): render CartSummary component in CartMain component when the cart is not empty

--- src/components/Shop/CartMain.jsx
+++ src/components/Shop/CartMain.jsx
@@ -3,10 +3,18 @@ import styled from "styled-components";
 import { useContext } from "react";
 import SavedProductsContext from "../../contexts/SavedProductsContext.jsx";
 import MessageWithImageBelow from "../Generic/MessageWithImageBelow.jsx";
+import CartSummary from "./CartSummary.jsx";
 import { Link } from "react-router-dom";
+import data from "../../assets/data.json";
+import { getCartSummary } from "../../utils/priceUtils.js";
+
+const baseShippingFee = data.baseShippingFee;
+const freeShippingAt = data.freeShippingAt;
 
 function CartMain({ className = "" }) {
-  const { cart } = useContext(SavedProductsContext);
+  const { cart, cartItems } = useContext(SavedProductsContext);
+
+  const cartSummaryData = getCartSummary(cart, baseShippingFee, freeShippingAt);
 
   return (
     <StyledMain className={className}>
@@ -16,7 +24,7 @@ function CartMain({ className = "" }) {
 
       {cart &&
         (cart.length > 0 ? (
-          "SHOW CART (TODO)"
+          <CartSummary {...{ ...cartSummaryData, cartItems }} />
         ) : (
           <MessageWithImageBelow imageUrl="/images/vector/empty-cart.jpg">
             Your cart is empty!
