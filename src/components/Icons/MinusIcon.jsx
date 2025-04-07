16d94c8517325351e9b070941863d597abb809ab
frarosset <37275581+frarosset@users.noreply.github.com>
2025-04-07 10:39:45 +0200

    feat(icons): add PlusIcon and MinusIcon components

---
+++ src/components/Icons/MinusIcon.jsx
@@ -0,0 +1,12 @@
+import Icon from "./Icon.jsx";
+import Minus from "../../assets/icons/minus.svg?react";
+
+function MinusIcon() {
+  return (
+    <Icon>
+      <Minus />
+    </Icon>
+  );
+}
+
+export default MinusIcon;
---
+++ src/components/Icons/PlusIcon.jsx
@@ -0,0 +1,12 @@
+import Icon from "./Icon.jsx";
+import Plus from "../../assets/icons/plus.svg?react";
+
+function PlusIcon() {
+  return (
+    <Icon>
+      <Plus />
+    </Icon>
+  );
+}
+
+export default PlusIcon;
