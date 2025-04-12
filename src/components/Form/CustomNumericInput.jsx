1f75eff0196d242823edd949dbb042a93d5a6f93
frarosset <37275581+frarosset@users.noreply.github.com>
2025-04-12 11:53:29 +0200

    feat(CustomNumericInput): renders an Input element with type="number" in CustomNumericInput component
    The handling of the typed value (setting the state based on the value,
    or handling out-of-bound values) is not implemented, yet.

--- src/components/Form/CustomNumericInput.jsx
+++ src/components/Form/CustomNumericInput.jsx
@@ -1,12 +1,19 @@
+import Input from "./Input.jsx";
 import PlusIcon from "../Icons/PlusIcon.jsx";
 import MinusIcon from "../Icons/MinusIcon.jsx";
 
+// Note: currently this works only with numbers with step=1
+const step = 1;
+
 function CustomNumericInput({
+  id,
+  name = id,
   value,
   min,
   max,
   decrementValueCallback,
   incrementValueCallback,
+  inputAriaLabel,
   decrementAriaLabel,
   incrementAriaLabel,
   className = "",
@@ -20,6 +27,17 @@ function CustomNumericInput({
       >
         <MinusIcon />
       </button>
+      <Input
+        id={id}
+        name={name}
+        type="number"
+        value={value}
+        min={min}
+        max={max}
+        step={step}
+        setOnBlur={true}
+        ariaLabel={inputAriaLabel}
+      />
       <button
         onClick={incrementValueCallback}
         disabled={value == max}
