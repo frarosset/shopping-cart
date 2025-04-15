e952b515a35db76ac5e9617d867e0d68d54b0be0
frarosset <37275581+frarosset@users.noreply.github.com>
2025-04-15 14:51:07 +0200

    feat(TrashIcon): add TrashIcon component

---
+++ src/components/Icons/TrashIcon.jsx
@@ -0,0 +1,12 @@
+import Icon from "./Icon.jsx";
+import Trash from "../../assets/icons/trash-simple.svg?react";
+
+function TrashIcon() {
+  return (
+    <Icon>
+      <Trash />
+    </Icon>
+  );
+}
+
+export default TrashIcon;
