7d3561e03a6543166822cd6e5412b192e3941595
frarosset <37275581+frarosset@users.noreply.github.com>
2025-04-15 16:53:46 +0200

    feat(CashRegisterIcon): add CashRegisterIcon component

---
+++ src/components/Icons/CashRegisterIcon.jsx
@@ -0,0 +1,12 @@
+import Icon from "./Icon.jsx";
+import CashRegister from "../../assets/icons/cash-register.svg?react";
+
+function CashRegisterIcon() {
+  return (
+    <Icon>
+      <CashRegister />
+    </Icon>
+  );
+}
+
+export default CashRegisterIcon;
