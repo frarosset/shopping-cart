89886a7fc2f717082cb21e43a422ad87a6be42c6
frarosset <37275581+frarosset@users.noreply.github.com>
2025-04-17 12:38:42 +0200

    style(CartProductList): add style to CartProductList component

--- src/components/Shop/CartProductList.jsx
+++ src/components/Shop/CartProductList.jsx
@@ -1,5 +1,6 @@
 import List from "../Generic/List.jsx";
 import CartProductItem from "./CartProductItem.jsx";
+import styled from "styled-components";
 
 function CartProductList({ productDataList, className = "" }) {
   const items = productDataList.map((productData) => ({
@@ -7,7 +8,19 @@ function CartProductList({ productDataList, className = "" }) {
     element: <CartProductItem productData={productData} />,
   }));
 
-  return <List className={className} items={items} />;
+  return <StyledList className={className} items={items} />;
 }
+
+const StyledList = styled(List)`
+  display: flex;
+  flex-direction: column;
+  gap: var(--cart-product-list-gap);
+  width: 100%;
+
+  li {
+    display: flex;
+    justify-content: center;
+  }
+`;
 
 export default CartProductList;
