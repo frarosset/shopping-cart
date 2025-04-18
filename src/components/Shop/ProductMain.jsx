d20eee62a7c1887f839df4c39191929bc380c99c
frarosset <37275581+frarosset@users.noreply.github.com>
2025-04-18 18:04:50 +0200

    feat(ProductMain): add ProductMain component
    The component renders a ProductFetch component to handle the fetching
    and display the fetching info for the product whose id is grabbed from
    the url.

---
+++ src/components/Shop/ProductMain.jsx
@@ -0,0 +1,23 @@
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import ProductFetch from "./ProductFetch.jsx";
+
+function ProductMain({ className = "" }) {
+  const { id } = useParams();
+
+  return (
+    <StyledMain className={className}>
+      <ProductFetch id={id} />
+    </StyledMain>
+  );
+}
+
+const StyledMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  gap: var(--page-gap);
+  align-items: center;
+  padding: var(--page-outlet-padding);
+`;
+
+export default ProductMain;
