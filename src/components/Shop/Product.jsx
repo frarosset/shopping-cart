5c73f1e5b022e7f2173cb85be962978767f09b67
frarosset <37275581+frarosset@users.noreply.github.com>
2025-04-19 10:19:32 +0200

    feat(Product): add Product component
    The component is based on ProductItem component, as it render the same
    basic element (except for the Thumbnail image and AddToCartButton
    button, that will be replaced with custom components).
    
    Other components will be added next, too.

---
+++ src/components/Shop/Product.jsx
@@ -0,0 +1,32 @@
+import {
+  Title,
+  PriceContainer,
+  RatingContainer,
+  DiscountPercentage,
+  AvailabilityStatus,
+  WishlistButton,
+  StyledRowContainer,
+} from "./StyledProductInfo.jsx";
+import CategoryItem from "./CategoryItem.jsx";
+import { HeadingLevelContextProvider } from "../../contexts/HeadingLevelContext.jsx";
+
+function Product({ productData, className = "" }) {
+  return (
+    <HeadingLevelContextProvider>
+      <div className={className}>
+        <CategoryItem {...productData} />
+        <StyledRowContainer>
+          <AvailabilityStatus product={productData} />
+          <DiscountPercentage {...productData} />
+        </StyledRowContainer>
+
+        <Title {...productData} />
+        <WishlistButton product={productData} />
+        <PriceContainer {...productData} />
+        <RatingContainer {...productData} />
+      </div>
+    </HeadingLevelContextProvider>
+  );
+}
+
+export default Product;
