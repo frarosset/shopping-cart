ac8bdfed7eab749a6ca3d5ba6c6383ea1729c30c
frarosset <37275581+frarosset@users.noreply.github.com>
2025-05-07 17:15:50 +0200

    style(Product): add style to Product component

--- src/components/Shop/Product.jsx
+++ src/components/Shop/Product.jsx
@@ -17,36 +17,111 @@ import SectionFromCategoryItem from "./SectionFromCategoryItem.jsx";
 import CategoryItem from "./CategoryItem.jsx";
 import { HeadingLevelContextProvider } from "../../contexts/HeadingLevelContext.jsx";
 import data from "../../assets/data.json";
+import styled from "styled-components";
 
 function Product({ productData, className = "" }) {
   return (
     <HeadingLevelContextProvider>
-      <div className={className}>
-        <div>
+      <StyledProduct className={className}>
+        <StyledLinksContainer>
           <SectionFromCategoryItem {...productData} />
           <span> / </span>
           <CategoryItem {...productData} />
-        </div>
-        <ImagesCarousel {...productData} />
-        <StyledRowContainer>
-          <AvailabilityStatus
-            product={productData}
-            ignoreStatusList={[data.availability.inStock]}
-          />
-          <DiscountPercentage {...productData} />
-        </StyledRowContainer>
-        <Title {...productData} />
-        <WishlistButton product={productData} />
-        <Description {...productData} />
-        <PriceContainer {...productData} />
-        <AddMultipleToCart product={productData} />
-        <RatingContainer {...productData} />
-        <PurchaseInfo product={productData} />
-        <Details {...productData} />
-        <ReviewList {...productData} />
-      </div>
+        </StyledLinksContainer>
+        <StyledProductImageAndInfoContainer>
+          <ImagesCarousel {...productData} />
+          <StyledProductInfoContainer>
+            <StyledRowContainer>
+              <AvailabilityStatus
+                product={productData}
+                ignoreStatusList={[data.availability.inStock]}
+              />
+              <DiscountPercentage {...productData} />
+            </StyledRowContainer>
+            <div>
+              <StyledRowContainerSpaceBetween>
+                <RatingContainer {...productData} />
+                <WishlistButton product={productData} />
+              </StyledRowContainerSpaceBetween>
+              <StyledTitle {...productData} />
+              <Description {...productData} />
+            </div>
+            <StyledPriceContainer {...productData} />
+            <AddMultipleToCart product={productData} />
+
+            <PurchaseInfo product={productData} />
+            <Details {...productData} />
+          </StyledProductInfoContainer>
+        </StyledProductImageAndInfoContainer>
+        <StyledReviewListContainer>
+          <ReviewList {...productData} />
+        </StyledReviewListContainer>
+      </StyledProduct>
     </HeadingLevelContextProvider>
   );
 }
+
+const StyledProduct = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: var(--product-main-gap);
+  align-items: center;
+  width: 100%;
+`;
+
+const StyledLinksContainer = styled.div`
+  text-align: center;
+
+  & > *:first-child {
+    text-transform: uppercase;
+    color: var(--col-highlight);
+  }
+`;
+
+const StyledProductImageAndInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: var(--product-main-gap);
+  width: 100%;
+
+  @media screen and (min-width: 960px) {
+    flex-direction: row;
+    align-items: flex-start;
+    padding-left: var(--page-padding-lr);
+    gap: 0;
+  }
+`;
+
+const StyledProductInfoContainer = styled.div`
+  padding: 0 var(--page-padding-lr);
+  max-width: var(--product-item-max-width);
+
+  display: flex;
+  flex-direction: column;
+  gap: 1lh;
+`;
+
+const StyledTitle = styled(Title)`
+  font-size: var(--heading-2-size);
+`;
+
+const StyledPriceContainer = styled(PriceContainer)`
+  * {
+    font-size: var(--heading-2-size);
+  }
+`;
+
+const StyledRowContainerSpaceBetween = styled(StyledRowContainer)`
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+
+const StyledReviewListContainer = styled.div`
+  padding: 0 var(--page-padding-lr);
+  max-width: var(--product-item-max-width);
+  width: 100%;
+`;
 
 export default Product;
