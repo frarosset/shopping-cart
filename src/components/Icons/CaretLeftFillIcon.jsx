148de709ab5a85f2624514364e2c31d2062b917a
frarosset <37275581+frarosset@users.noreply.github.com>
2025-05-08 14:19:18 +0200

    feat(CaretLeftFillIcon, CaretRightFillIcon): add CaretLeftFillIcon, CaretRightFillIcon components
    The previous fix for Carousel component does not solve the problem of
    arrow characters not displaying correctly in Android browsers. The
    characters will be replaced by these svg icons.

---
+++ src/components/Icons/CaretLeftFillIcon.jsx
@@ -0,0 +1,12 @@
+import Icon from "./Icon.jsx";
+import CaretLeftFill from "../../assets/icons/caret-left-fill.svg?react";
+
+function CaretLeftFillIcon() {
+  return (
+    <Icon>
+      <CaretLeftFill />
+    </Icon>
+  );
+}
+
+export default CaretLeftFillIcon;
---
+++ src/components/Icons/CaretRightFillIcon.jsx
@@ -0,0 +1,12 @@
+import Icon from "./Icon.jsx";
+import CaretRightFill from "../../assets/icons/caret-right-fill.svg?react";
+
+function CaretRightFillIcon() {
+  return (
+    <Icon>
+      <CaretRightFill />
+    </Icon>
+  );
+}
+
+export default CaretRightFillIcon;
